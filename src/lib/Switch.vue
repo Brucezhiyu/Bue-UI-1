d1bcd72d55432ab17fb32d641709895e9605ca6a
Brucezhiyu <lushuke5@gmail.com>
2022-05-18 23:06:46 +0800

    完成了switch 按钮

---
+++ src/lib/Switch.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button><span></span></button>
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+button{
+height: 22px;
+  width: 44px;
+  border: none;
+  background: dodgerblue;
+  border-radius: 11px;
+  position: relative;
+}
+span{
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: 18px;
+  width: 18px;
+  border-radius: 9px;
+  background: white;
+}
+</style>
