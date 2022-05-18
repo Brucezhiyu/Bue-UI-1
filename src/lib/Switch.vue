b118eddbb5fe5c5875bfa0ace5626657bef3c11c
Brucezhiyu <lushuke5@gmail.com>
2022-05-18 23:22:53 +0800

    按钮可以点击切换了

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,32 +1,46 @@
 <template>
   <div>
-    <button><span></span></button>
+    <button @click="toggle" :class="{checked:checked}"><span></span></button>
   </div>
 </template>
 
 <script lang="ts">
+import {ref} from 'vue';
 
+export default {
+  setup() {
+    const checked = ref(false);
+    const toggle=()=>{
+      checked.value=!checked.value;
+    }
+    return {checked,toggle};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-button{
-height: 22px;
+button {
+  height: 22px;
   width: 44px;
   border: none;
-  background: dodgerblue;
+  background: lightgrey;
   border-radius: 11px;
   position: relative;
 }
-span{
+
+span {
   position: absolute;
   top: 2px;
-  left:2px;
+  left: 2px;
   height: 18px;
   width: 18px;
   border-radius: 9px;
   background: white;
 }
-button:hover>span{
+button.checked{
+  background: dodgerblue;
+}
+button.checked > span {
   left: calc(100% - 20px);
 
 }
