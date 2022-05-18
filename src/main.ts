4c02cde6cc4b723963c5ef3291e55cb5ff7006d5
Brucezhiyu <lushuke5@gmail.com>
2022-05-18 13:07:45 +0800

    完成了html搭建

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,20 @@ import {createApp} from 'vue';
 import App from './App.vue';
 import './index.css';
 import {createWebHashHistory, createRouter} from 'vue-router';
-import Frank from './components/Frank.vue'
-import Frank2 from './components/Frank2.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+
 const history = createWebHashHistory();
 const router = createRouter({
     history: history,
     routes: [
         {
             path: '/',
-            component: Frank
+            component: Home
         },
         {
-            path:'/xxx',
-            component:Frank2
+            path:'/doc',
+            component:Doc
         }
     ]
 });
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <div class="topnav">
+    <div class="logo"> </div>
+    <div class="menu"></div>
+  </div>
+  <div class="banner">
+    <h1>guluUI</h1>
+    <h2>一个哇塞的 UI 框架</h2>
+    <p class="actions">
+      <a>Github</a>
+      <a>开始</a>
+    </p>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.topnav{border: 1px solid red}
+</style>
