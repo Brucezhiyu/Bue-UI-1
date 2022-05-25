0fa17a78873e0bdf5087ee54d961d053f049eb0f
Brucezhiyu <lushuke5@gmail.com>
2022-05-25 14:57:52 +0800

    完成了响应式布局

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <router-link to="/" class="logo">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-icon-b"></use>
+      </svg>
+      Bue UI
+    </router-link>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li>
+        <router-link to="/doc">文档</router-link>
+      </li>
+
     </ul>
     <span class="toggleAside" @click="toggleMenu"></span>
   </div>
@@ -30,14 +37,23 @@ export default {
   background: white;
   display: flex;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
+  color: rgba(2, 39, 61);
   padding: 20px;
   z-index: 2;
   justify-content: center;
   align-items: center;
 
   > .logo {
-    max-width: 6em;
+    &:hover {
+      text-decoration: none;
+    }
+    max-width: 8em;
     margin-right: auto;
+    >.icon{
+      width: 30px;
+     height: 20px;
+    }
+
   }
 
   > .menu {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,12 @@
   <div class="topnavAndBanner">
     <Topnav/>
     <div class="banner">
-      <h1 class="bue1">Bue UI</h1>
+      <h1 class="bue1">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-icon-b"></use>
+        </svg>
+        Bue UI
+      </h1>
       <h2 class="bue2">一个很哇塞的 UI 组件库</h2>
       <div class="actions">
         <a href="https://github.com/Brucezhiyu/Bue-UI-1">
@@ -18,29 +23,34 @@
           </svg>
         </router-link>
       </div>
-      <ul class="icon-border">
-        <li>
-        <svg class="icon2" aria-hidden="true">
-          <use xlink:href="#icon-Vue"></use>
-        </svg>
-        <h3>基于 Vue 3</h3>
-        <p>骄傲地使用了 Vue 3 Composition API</p>
-        </li>
-        <li>
-        <svg class="icon2" aria-hidden="true">
-          <use xlink:href="#icon-typescript"></use>
-        </svg>
-        <h3>基于 TypeScript </h3>
-        <p>源代码采用 TypeScript 书写（非严格检查）</p>
-        </li>
-        <li>
-          <svg class="icon2" aria-hidden="true">
-          <use xlink:href="#icon-light"></use>
-        </svg>
-        <h3>代码易读</h3>
-        <p>每个组件的源代码都极其简洁</p>
-        </li>
-      </ul>
+      <div class="icon-border">
+        <ul>
+          <li>
+            <svg class="icon2" aria-hidden="true">
+              <use xlink:href="#icon-Vue"></use>
+            </svg>
+            <h3>基于 Vue 3</h3>
+            <p>骄傲地使用了 Vue 3 Composition API</p>
+          </li>
+          <li>
+            <svg class="icon2" aria-hidden="true">
+              <use xlink:href="#icon-typescript"></use>
+            </svg>
+            <h3>基于 TypeScript </h3>
+            <p>源代码采用 TypeScript 书写（非严格检查）</p>
+          </li>
+          <li>
+            <svg class="icon2" aria-hidden="true">
+              <use xlink:href="#icon-light"></use>
+            </svg>
+            <h3>代码易读</h3>
+            <p>每个组件的源代码都极其简洁</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="">
+
     </div>
   </div>
 </template>
@@ -55,22 +65,53 @@ export default {
 
 <style lang="scss" scoped>
 .topnavAndBanner {
-  overflow: hidden;
-  height: 100vh;
+  overflow-x: hidden;
   background: linear-gradient(180deg, rgba(255, 255, 255, 1) 0%, rgba(243, 241, 240, 1) 100%, rgba(100, 50, 170, 0) 100%);
 
 }
 
 .icon-border {
-  border-top: 1px solid lightgrey;
+  margin: 40px 0;
+  width: 300px;
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  >h3 {
-    font-size: 40px;
+  @media (min-width: 600px) {
+    width: 600px;
   }
-  >p {
+  @media (min-width: 900px) {
+    width: 900px;
+  }
+
+  > ul {
+    border-top: 1px solid lightgrey;
+    display: flex;
+    flex-wrap: wrap;
     font-size: 6px;
+
+    > li {
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 50px auto;
+      grid-template-rows: 20px auto;
+
+      > svg {
+        grid-area: icon;
+      }
+
+      > h3 {
+        margin-top: 20px;
+        grid-area: title;
+        font-size: 18px;
+      }
+
+      > p {
+        margin-top: 23px;
+        grid-area: text
+      }
+    }
   }
 
   .icon2 {
@@ -81,19 +122,33 @@ export default {
 }
 
 .banner {
-  padding: 150px 80px;
+  padding-top: 130px;
   display: flex;
   color: rgba(2, 39, 61);
-  justify-content: flex-start;
-  align-items: flex-start;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
+  @media (min-width: 600px) {
+    padding-top: 150px;
+    padding-left: 150px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-direction: column;
+  }
 
   > .bue1 {
-    font-size: 80px
+    font-size: 60px;
+    @media (min-width: 600px) {
+      font-size: 80px;
+    }
   }
 
   > .bue2 {
-    font-size: 40px;
+    font-size: 20px;
+    @media (min-width: 600px) {
+      font-size: 40px;
+    }
   }
 
   > .actions {
@@ -114,8 +169,10 @@ export default {
       background: rgba(2, 39, 61);
       line-height: 32px;
       border-radius: 4px;
-      padding: 10px 150px;
-
+      padding: 10px 60px;
+@media (min-width: 600px) {
+  padding: 10px 150px;
+}
       &:hover {
         text-decoration: none;
       }
