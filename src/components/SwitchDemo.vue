30e9ae07b111b898c505a2b465d4b2f772b78dfb
Brucezhiyu <lushuke5@gmail.com>
2022-05-25 18:01:49 +0800

    高亮美化代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch1Demo.__sourceCode}}</pre>
+        <pre v-html="Prism.highlight(Switch1Demo.__sourceCode,Prism.languages.html,'html')"/>
       </div>
     </div>
     <div class="demo">
@@ -22,7 +22,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch2Demo.__sourceCode}}</pre>
+        <pre  v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html,'html')"/>
       </div>
     </div>
   </div>
@@ -34,12 +34,14 @@ import {ref} from 'vue';
 import Button from '../lib/Button.vue';
 import Switch1Demo from'./Switch1.demo.vue'
 import Switch2Demo from'./Switch2.demo.vue'
-
+import 'prismjs'
+import 'prismjs/themes/prism-coy.css'
+const Prism=(window as any).Prism
 export default {
   components: {Switch1Demo,Switch2Demo, Switch, Button},
   setup() {
     const bool = ref(false);
-    return {bool,Switch1Demo, Switch2Demo};
+    return {bool,Prism,Switch1Demo, Switch2Demo};
   }
 };
 </script>
